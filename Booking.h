a7452c2a6f2291c336315ead6036bbd8450d3b1c
hatifmujahid <hatifmujahid08@gmail.com>
2021-05-10 02:33:28 +0500

    dfsf

--- Booking.h
+++ Booking.h
@@ -1,7 +1,29 @@
 #ifndef Final_Project
 #define Final_Project
 class Airline;
-class Customer;
+class Customer : virtual public Person // hatif
+{
+private:
+protected:
+    string f_name, l_name;
+
+public:
+    string u, p;
+    friend class Booking;
+    friend class Staff;
+    void set_fname(string f_name);
+    const string get_fname();
+    void set_lname(char l_name);
+    const string get_lname();
+    void menu();
+    void signup();
+
+    void signin(string *user, string *pass);
+    void customer_menu();
+    void check_flight();
+    void book_flight();
+    void refund();
+};
 class Booking: protected Airline, public Customer
 {
     int ticket_ID;
--- Final_Project.cpp
+++ Final_Project.cpp
@@ -8,11 +8,8 @@
 #include <cstdio>
 #include <ctime>
 #include <string>
-#include"Booking.h"
-//ios::in input
-//ios::out output
-//ofstream output to file
-//ifstream input from file
+#include<sstream>
+
 using namespace std;
 void loading_screen()
 {
@@ -180,6 +177,76 @@ public:
     }
     void airline_menu()
     {
+    }
+};
+
+class Booking : public Airline, public Customer //mohtada
+{
+    Airline a1;
+    int ticket_ID;
+    string c_name, c_email, c_airline;
+    int c_id;
+
+protected:
+    void show()
+    {
+        cout << "\n\t\tTicket\nName: " << c_name << "Email: " << c_email << "ID: " << c_id << "Airline: " << c_airline << endl;
+    }
+
+public:
+    static int t_id;
+    const int get_ticket_id()
+    {
+        return ticket_ID;
+    }
+    Booking()
+    {
+        t_id++;
+        ticket_ID = t_id;
+    }
+
+    void get_data()
+    {
+        c_name = get_fname();
+        c_id = get_ID();
+        c_email = get_email();
+        c_airline = Airline::get_name();
+    }
+    void menu()
+    {
+        cout << "___________________________MENU BOOKING_______________________________\n ";
+    }
+    void book_tickets()
+    {
+        Booking a;
+        int choice;
+        loading_screen();
+        cout << "List of airlines: ";
+        Airline::showAirlines();
+        cout << "Enter ID of the Airline choosen: ";
+        cin >> choice;
+        reading(choice);
+        ofstream fp("ticket.dat", ios::app | ios::binary);
+        fp.write((char *)&a, sizeof(a));
+        fp.close();
+    }
+    void reading(int c)
+    {
+        ifstream fpt("airline.dat", ios::in | ios::binary);
+        while (1)
+        {
+            fpt.read((char *)&a1, sizeof(Airline));
+            if (a1.get_ID() == c)
+            {
+                break;
+            }
+            else if (fpt.eof())
+            {
+                cout << "\nChoice not found\n";
+                break;
+            }
+        }
+        fpt.close();
     }
 };
 class Customer : virtual public Person // hatif
@@ -684,75 +751,7 @@ public:
     }
 };
 class Ticket;
-class Booking : protected Airline, public Customer //mohtada
-{
-    Airline a1;
-    int ticket_ID;
-    string c_name, c_email, c_airline;
-    int c_id;
 
-protected:
-    void show()
-    {
-        cout << "\n\t\tTicket\nName: " << c_name << "Email: " << c_email << "ID: " << c_id << "Airline: " << c_airline << endl;
-    }
-
-public:
-    static int t_id;
-    const int get_ticket_id()
-    {
-        return ticket_ID;
-    }
-    Booking()
-    {
-        t_id++;
-        ticket_ID = t_id;
-    }
-
-    void get_data()
-    {
-        c_name = get_fname();
-        c_id = get_ID();
-        c_email = get_email();
-        c_airline = Airline::get_name();
-    }
-    void menu()
-    {
-        cout << "___________________________MENU BOOKING_______________________________\n ";
-    }
-    void book_tickets()
-    {
-        Booking a;
-        int choice;
-        loading_screen();
-        cout << "List of airlines: ";
-        Airline::showAirlines();
-        cout << "Enter ID of the Airline choosen: ";
-        cin >> choice;
-        reading(choice);
-        ofstream fp("ticket.dat", ios::app | ios::binary);
-        fp.write((char *)&a, sizeof(a));
-        fp.close();
-    }
-    void reading(int c)
-    {
-        ifstream fpt("airline.dat", ios::in | ios::binary);
-        while (1)
-        {
-            fpt.read((char *)&a1, sizeof(Airline));
-            if (a1.get_ID() == c)
-            {
-                break;
-            }
-            else if (fpt.eof())
-            {
-                cout << "\nChoice not found\n";
-                break;
-            }
-        }
-        fpt.close();
-    }
-};
 class Ticket : protected Booking //printing ticket //mohtada
 {
 public:
