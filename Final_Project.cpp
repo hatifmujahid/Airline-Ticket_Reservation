dcdb9305d88eeb35396be3450d1912af83369390
hatifmujahid <hatifmujahid08@gmail.com>
2021-05-10 02:55:27 +0500

    ads

--- Final_Project.cpp
+++ Final_Project.cpp
@@ -23,10 +23,12 @@ void loading_screen()
     Sleep(30);
     system("cls");
 }
-
+class Staff;
+class Booking;
 class HolidayPackage;
 class Ticket;
-class Staff;
+class Customer;
+
 class ui //user interface class
 {
 public:
@@ -180,75 +182,7 @@ public:
     }
 };
 
-class Booking : public Airline, public Customer //mohtada
-{
-    Airline a1;
-    int ticket_ID;
-    string c_name, c_email, c_airline;
-    int c_id;
 
-protected:
-    void show()
-    {
-        cout << "\n\t\tTicket\nName: " << c_name << "Email: " << c_email << "ID: " << c_id << "Airline: " << c_airline << endl;
-    }
-
-public:
-    static int t_id;
-    const int get_ticket_id()
-    {
-        return ticket_ID;
-    }
-    Booking()
-    {
-        t_id++;
-        ticket_ID = t_id;
-    }
-
-    void get_data()
-    {
-        c_name = get_fname();
-        c_id = get_ID();
-        c_email = get_email();
-        c_airline = Airline::get_name();
-    }
-    void menu()
-    {
-        cout << "___________________________MENU BOOKING_______________________________\n ";
-    }
-    void book_tickets()
-    {
-        Booking a;
-        int choice;
-        loading_screen();
-        cout << "List of airlines: ";
-        Airline::showAirlines();
-        cout << "Enter ID of the Airline choosen: ";
-        cin >> choice;
-        reading(choice);
-        ofstream fp("ticket.dat", ios::app | ios::binary);
-        fp.write((char *)&a, sizeof(a));
-        fp.close();
-    }
-    void reading(int c)
-    {
-        ifstream fpt("airline.dat", ios::in | ios::binary);
-        while (1)
-        {
-            fpt.read((char *)&a1, sizeof(Airline));
-            if (a1.get_ID() == c)
-            {
-                break;
-            }
-            else if (fpt.eof())
-            {
-                cout << "\nChoice not found\n";
-                break;
-            }
-        }
-        fpt.close();
-    }
-};
 class Customer : virtual public Person // hatif
 {
 private:
@@ -339,14 +273,13 @@ public:
     }
     void book_flight()
     {
-        Booking a;
-        a.menu();
+        Booking::menu()
         customer_menu();
     }
     void refund()
     {
-        Staff s;
-        s.menu();
+        Staff *s;
+        s->menu();
         customer_menu();
     }
 };
@@ -750,7 +683,76 @@ public:
         }
     }
 };
-class Ticket;
+
+class Booking : public Airline, public Customer //mohtada
+{
+    Airline a1;
+    int ticket_ID;
+    string c_name, c_email, c_airline;
+    int c_id;
+
+protected:
+    void show()
+    {
+        cout << "\n\t\tTicket\nName: " << c_name << "Email: " << c_email << "ID: " << c_id << "Airline: " << c_airline << endl;
+    }
+
+public:
+    static int t_id;
+    const int get_ticket_id()
+    {
+        return ticket_ID;
+    }
+    Booking()
+    {
+        t_id++;
+        ticket_ID = t_id;
+    }
+
+    void get_data()
+    {
+        c_name = get_fname();
+        c_id = get_ID();
+        c_email = get_email();
+        c_airline = Airline::get_name();
+    }
+    void menu()
+    {
+        cout << "___________________________MENU BOOKING_______________________________\n ";
+    }
+    void book_tickets()
+    {
+        Booking a;
+        int choice;
+        loading_screen();
+        cout << "List of airlines: ";
+        Airline::showAirlines();
+        cout << "Enter ID of the Airline choosen: ";
+        cin >> choice;
+        reading(choice);
+        ofstream fp("ticket.dat", ios::app | ios::binary);
+        fp.write((char *)&a, sizeof(a));
+        fp.close();
+    }
+    void reading(int c)
+    {
+        ifstream fpt("airline.dat", ios::in | ios::binary);
+        while (1)
+        {
+            fpt.read((char *)&a1, sizeof(Airline));
+            if (a1.get_ID() == c)
+            {
+                break;
+            }
+            else if (fpt.eof())
+            {
+                cout << "\nChoice not found\n";
+                break;
+            }
+        }
+        fpt.close();
+    }
+};
 
 class Ticket : protected Booking //printing ticket //mohtada
 {
